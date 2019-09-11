f8449c92c2a08674353d7826cd62dd2dd197196e
Horacio Gutierrez <horacio.estevez@gmail.com>
2019-09-11 12:07:23 -0300

    Implementacion de props y state

--- src/ui/layout/App.js
+++ src/ui/layout/App.js
@@ -4,15 +4,25 @@ import Footer from "./Footer"
 import Main from "./Main"
 
 class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            links : ["perfil","portfolio","contacto"]
+        }
+    }
+    
     render(){
+        let {links} = this.state
         return (
             <>
-                <Header/>
-                <Main/>
-                <Footer/>
+            <Header links={links}/>
+            <Main/>
+            <Footer/>
             </>     
         ) 
     }
 }
 
 export default App
+
--- src/ui/layout/Header.js
+++ src/ui/layout/Header.js
@@ -2,13 +2,12 @@ import React , {Component} from "react"
 
 class Header extends Component {
     render(){
+        let {links} = this.props
         return(
             <header>
                 <h1>SPA</h1>
                 <nav>
-                    <a href="#">link</a>
-                    <a href="#">link</a>
-                    <a href="#">link</a>
+                {links.map(link=><a href="#">{link}</a>)}
                 </nav>
             </header>
         )
