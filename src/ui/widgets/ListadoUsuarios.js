4e8f65f980cccb6560d40f5c0001ec2461b2f30e
Horacio Gutierrez <horacio.estevez@gmail.com>
2019-09-20 09:51:23 -0300

    Solucion del punto 2 : Agregar dos botones a cada item de lista

--- src/ui/widgets/ListadoUsuarios.js
+++ src/ui/widgets/ListadoUsuarios.js
@@ -8,7 +8,13 @@ class ListadoUsuarios extends Component {
             <ul>
             {usuarios.length 
             ? usuarios.map((usuario,i)=> 
-                <li key={i}>{usuario.nombre} {usuario.apellido}</li> 
+                <li key={i}>
+                    <p>{usuario.nombre} {usuario.apellido}</p>
+                    <div>
+                        <button>editar</button>
+                        <button>borrar</button>
+                    </div>
+                </li> 
             ) 
             : <li>No hay usuarios</li> }
             </ul>
